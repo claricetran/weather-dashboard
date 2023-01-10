6150389f65562a4b11f85d08617ff8cb003de47b
Clarice Tran <tran.clarice@gmail.com>
2023-01-10 03:23:22 -0800

    Worked on framework for site in html

--- README.md
+++ README.md
@@ -25,6 +25,8 @@ List your collaborators, if any, with links to their GitHub profiles.
 If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
 
 If you followed tutorials, include links to those here as well.
+[Regular Expressions](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions)
+[Regular Expression for Latitude and Longitude](https://ihateregex.io/expr/lat-long/)
 
 ## License
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
 var OpenWeatherKey = "bd9f20ec53ac9e675acad3ce4fc3aa3d";
+var city;
+var searchEl = document.getElementByID("search");
+// checks if latitude longitude format was put in
+
+// Regular Expression for lat long to implement later.
+// var latLongRE = /^((\-?|\+?)?\d+(\.\d+)?),\s*((\-?|\+?)?\d+(\.\d+)?)$/;
+// var requestUrl = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + OpenWeatherKey;
+
+// fetch(requestUrl).then(function (response) {});
+
+// Get location user submitted for search
